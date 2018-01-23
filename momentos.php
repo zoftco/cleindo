592ea96a5c4f3e047d05a4889d3e5e069f3747b0
Fernando Chavez Gomes da Silva (ICE) <fernandochavez@zoftco.com>
2018-01-23 13:16:09 -0400

    Complementos a Momentos CLEIN

--- momentos.php
+++ momentos.php
@@ -20,45 +20,47 @@ if(isset($_SESSION['user_name'])) {
         <img src="img/momentos/ACTIVIDAD-DE-INTEGRACION.jpg" width="100%" />
     </div>
     <div class="container">
-
-        <h4 class="title">Actividad de integración</h4>
         <p>Con el fin de fomentar el trabajo en equipo y intercambio social y cultural, el CLEIN República Dominicana 2018 pondrá aprueba las habilidades físicas, creativas y sociales en una actividad deportiva recreativa.</p>
     </div>
     <div class="container-fluid">
         <img src="img/momentos/ACT.-SOCIAL.jpg" width="100%" />
     </div>
     <div class="container">
-        <h4 class="title">Actividad Social</h4>
         <p>Diferentes grupos de participantes del CLEIN República Dominicana  tendrán la oportunidad de vivir la experiencia de realizar una actividad social.</p>
     </div>
     <div class="container-fluid">
         <img src="img/momentos/GALA-DE-APERTURA.jpg" width="100%" />
     </div>
     <div class="container">
-        <h4 class="title">Gala de Apertura</h4>
         <p>Para recibir a las delegaciones al evento más esperado del año, CLEIN República Dominicana 2018 abrirá sus puertas en uno de los salones más prestigiosos del país y con esto, en un entorno lleno de magia y empatía, dar inicio a todas las actividades del congreso.</p>
     </div>
     <div class="container-fluid">
         <img src="img/momentos/FIESTA-DE-LAS-NACIONES.jpg" width="100%" />
     </div>
     <div class="container">
-        <h4 class="title">Fiesta de las Naciones</h4>
         <p>En esta integración, cada país podrá presentar su cultura de la siguiente forma:</p>
         <p>-Música.</p>
         <p>-Bailes.</p>
         <p>-Artesanías.</p>
         <p>-Platos y bebidas típicas.</p>
         <p>-Cultura en general.</p>
-        <h4 class="title">Fiesta Dominicana</h4>
+    </div>
+    <div class="container-fluid">
+        <img src="img/momentos/FIESTA-DOMINICANA.jpg" width="100%" />
+    </div>
+    <div class="container">
         <p>Visitar el Corazón del Caribe te invita a conocer a detalle su cultura, degustar su gastronomía, historia, y demás atractivos; por ello, esta fiesta tiene como objetivo hacer que cada asistente sienta la vibra dominicana en todo su esplendor.</p>
-        <h4 class="title">Party de Marquesina</h4>
+    </div>
+    <div class="container-fluid">
+        <img src="img/momentos/PARTY-DE-MARQUESINA.jpg" width="100%" />
+    </div>
+    <div class="container">
         <p>Una fiesta creada para dar a conocer las partes más urbana del país, con la exclusividad y carisma que sólo Republica Dominicana sabe brindar.</p>
     </div>
     <div class="container-fluid">
         <img src="img/momentos/CENA-DE-CLAUSURA-B.jpg" width="100%" />
     </div>
     <div class="container">
-        <h4 class="title">Cena de Clausura</h4>
         <p>El CLEIN República Dominicana 2018 culminará exitosamente con una ceremonia de clausura especial. Contará con la Ponencia Cultural, las premiaciones a las mejores ponencias estudiantiles y profesionales, reconocimiento a los charlistas magistrales y un delicioso buffet.</p>
     </div>
 </section>
