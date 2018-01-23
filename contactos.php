8b7defcd2f8a427489ab22a3f0d4267a7ea6753f
Fernando Chavez Gomes da Silva (ICE) <fernandochavez@zoftco.com>
2018-01-23 13:14:08 -0400

    Contactos con enlace Whatsapp

--- contactos.php
+++ contactos.php
@@ -17,200 +17,211 @@ Bienvenido/a <strong><?php echo $_SESSION['user_name'];?></strong> <!-- <a href=
 <section id="main">
 	<div class="container">
 			<h4 class="title">Contacta al delegado de tu país</h4>
-            <table class="table table-bordered">
+            <table class="table table-bordered text-center">
                 <thead>
                     <tr >
                         <th scope="col" width="33%">
-                            <p><b><span>Pais</span></b></p>
+                            <b>Pais</b>
                         </th>
                         <th scope="col" width="33%">
-                            <p><b><span>Nombre del delegado</span></b></p>
+                            <b>Nombre del delegado</b>
                         </th>
                         <th scope="col" width="33%">
-                            <p><b><span>Contacto</span></b></p>
+                            <b>Contacto</b>
                         </th>
                     </tr>
                 </thead>
                 <tr >
                     <td>
-                        <p><span>Nicaragua</span></p>
+                        Nicaragua
                     </td>
                     <td >
-                        <p><span>Ivan Acevedo</span></p>
+                        Ivan Acevedo
                     </td>
                     <td>
-                        <p><span>(+505)82129207</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=50582129207">(+505) 8212 9207</a>
                     </td>
                 </tr>
                 <tr >
                     <td>
-                        <p><span>Argentina</span></p>
+                        Argentina
                     </td>
                     <td >
-                        <p><span>Olivia Brugna</span></p>
+                        Olivia Brugna
                     </td>
                     <td>
-                        <p><span>(+5492)215379063</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=5492215379063">(+5492) 21 537 9063</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Peru</span></p>
+                        Peru
                     </td>
                     <td >
-                        <p><span>Yosselyn V.</span></p>
+                        Yosselyn V.
                     </td>
                     <td>
-                        <p><span>(+51)989302500</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=51989302500">(+51) 98 930 2500</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>El Salvador</span></p>
+                        El Salvador
                     </td>
                     <td >
-                        <p><span>Daniel Salgado</span></p>
+                        Daniel Salgado
                     </td>
                     <td>
-                        <p><span>(+503)79861300</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=50379861300">(+503) 7986 1300</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Uruguay</span></p>
+                        Uruguay
                     </td>
                     <td >
-                        <p><span>Richard Uruguay</span></p>
+                        Richard Uruguay
                     </td>
                     <td>
-                        <p><span>(+598)99380556</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=59899380556">(+598) 9938 0556</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Chile</span></p>
+                        Chile
                     </td>
                     <td >
-                        <p><span>Bianca Salinas</span></p>
+                        Bianca Salinas
                     </td>
                     <td>
-                        <p><span>(+569)33044814</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=56933044814">(+569) 3304 4814</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Bolivia</span></p>
+                        Bolivia
                     </td>
                     <td >
-                        <p><span>Cinthia Lopez</span></p>
+                        Cinthia Lopez
                     </td>
                     <td>
-                        <p><span>(+591)72221837</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=59172221837">(+591) 722 21837</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Colombia</span></p>
+                        Colombia
                     </td>
                     <td >
-                        <p><span>Victor Mantilla</span></p>
+                        Victor Mantilla
                     </td>
                     <td>
-                        <p><span>(+57)3132066535</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=573132066535">(+57) 313 206 6535</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Cuba</span></p>
+                        Cuba
                     </td>
                     <td >
-                        <p><span>Danhiz Diaz Pereira</span></p>
+                        Danhiz Diaz Pereira
                     </td>
                     <td>
-                        <p><span>(+53)53474126</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=5353474126">(+53) 5347 4126</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Brazil</span></p>
+                        Brazil
                     </td>
                     <td >
-                        <p><span>Livia Maia Braga</span></p>
+                        Livia Maia Braga
                     </td>
                     <td>
-                        <p><span>(+55)4198184039</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=554198184039">(+55) 419 818 4039</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Venezuela</span></p>
+                        Venezuela
                     </td>
                     <td >
-                        <p><span>Luis Gonzalez</span></p>
+                        Luis Gonzalez
                     </td>
                     <td>
-                        <p><span>(+58)4241661142</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=584241661142">(+58) 424 166 1142</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Costa Rica</span></p>
+                        Costa Rica
                     </td>
                     <td >
-                        <p><span>Keyler Jimenez</span></p>
+                        Keyler Jimenez
                     </td>
                     <td>
-                        <p><span>(+506)83145401</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=50683145401">(+506) 8314 5401</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Honduras</span></p>
+                        Honduras
                     </td>
                     <td >
-                        <p><span>Randolfo Ayala</span></p>
+                        Randolfo Ayala
                     </td>
                     <td>
-                        <p><span>(+504)32380409</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=50432380409">(+504) 3238 0409</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Guatemala</span></p>
+                        Guatemala
                     </td>
                     <td >
-                        <p><span>Adner Zuñiga</span></p>
+                        Abner Zuñiga
                     </td>
                     <td>
-                        <p><span>(+502)55789684</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=50255789684">(+502) 5578 9684</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Mexico</span></p>
+                        Mexico
                     </td>
                     <td >
-                        <p><span>Sergio Medina</span></p>
+                        Sergio Medina
                     </td>
                     <td>
-                        <p><span>(+52)155514917983</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=52155514917983">(+52) 15 551 491 7983</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <p><span>Ecuador</span></p>
+                        Ecuador
                     </td>
                     <td >
-                        <p><span>Evelyn Fonseca</span></p>
+                        Evelyn Fonseca
                     </td>
                     <td>
-                        <p><span>(+593)995281199</span></p>
+                        <a href="https://api.whatsapp.com/send?phone=593995281199">(+593)99 528 1199</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        República Dominicana
+                    </td>
+                    <td >
+                        John Paul Guzman
+                    </td>
+                    <td>
+                        <a href="https://api.whatsapp.com/send?phone=18293409452">(+1)829 340 9542</a>
                     </td>
                 </tr>
                 <tr>
                     <td colspan="3">
-                        <p><span>*Para Paraguay, Puerto Rico y Panamá
-  contactar al (+1)8294431870 </span></p>
+                        *Para Paraguay, Puerto Rico y Panamá
+  contactar al <a href="https://api.whatsapp.com/send?phone=18294431870">(+1)8294431870</a>
                     </td>
                 </tr>
             </table>
