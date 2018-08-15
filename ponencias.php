425cab62ffa1b2a3cf04ff306c0f4bb734052b60
Fernando Chavez Gomes da Silva (ICE) <fernandochavez@zoftco.com>
2018-08-15 12:49:11 -0400

    Cambio Cronograma Concurso Ponencias

--- ponencias.php
+++ ponencias.php
@@ -11,7 +11,7 @@
         <p>El CLEIN República Dominicana 2018 presenta el concurso oficial de ponencias estudiantiles y profesionales. El objetivo del mismo es involucrar a los asistentes en el desarrollo de las actividades académicas del congreso aportando con sus investigaciones y conocimientos a los demás participantes.</p>
         <p> Accede al manual de Ponencias para conocer más sobre requisitos, beneficios y criterios de evaluación.
         </p>
-        <img src="img/ofertaacademica/cronograma.PNG" alt="Cronograma de Concurso de Ponencias Estudiantiles y Profesionales CLEIN República Dominicana 2018" width="100%" />
+        <img src="img/ofertaacademica/cronogramapop.png" alt="Cronograma de Concurso de Ponencias Estudiantiles y Profesionales CLEIN República Dominicana 2018" width="100%" />
         <nav class="nav nav-pills nav-justified">
             <a class="nav-item nav-link" href="https://issuu.com/cleinrd/docs/manual_de_ponencias_estudiantiles_y" target="_blank">CONCURSO DE PONENCIAS</a>
             <a class="nav-item nav-link" href="https://drive.google.com/file/d/1HKpqV8TGQkihHk4C5cI69Nxkj8VGv9uY/view" target="_blank">FORMATO DE PONENCIAS</a>
